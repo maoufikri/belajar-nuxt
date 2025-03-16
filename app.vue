094cdf0f137e6649d3ed41dbd47b1fe9465f83b7
maoufikri <fikrimuhammad38993@gmail.com>
2025-03-16 16:43:06 +0700

    counter app

--- app.vue
+++ app.vue
@@ -1,13 +1,58 @@
 <template>
-  <h1>Maou sama 73</h1>
+  <div class="container">
+    <h1>Counter APP</h1>
+    <div v-if="!submitted">
+      <input v-model="nama" placeholder="Masukkan Nama" type="text">
+      <button @click="submitNama">submit</button>
+    </div>
+
+    <div v-if="submitted">
+      <h1>Halo {{ nama }} silakan jalankan conter</h1>
+      <div>
+        <h1 :style="{ color: warna}">{{ counter }}</h1>
+        <button @click="kurang">-</button>
+        <button @click="tambah">+</button>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script setup>
+import { ref, computed } from "vue";
 
+const nama = ref("");
+const submitted = ref(false);
+const counter = ref(0);
+
+
+const warna = computed(() => (counter.value % 2 === 0 ? "red" : "blue"));
+
+const submitNama = () => {
+  if (nama.value.trim()) submitted.value = true;
+};
+
+const tambah = () => counter.value++;
+const kurang = () => counter.value--;
 </script>
 
 <style>
-  h1{
-    color: red;
-  }
+.container {
+  text-align: start;
+  margin-top: 50px;
+}
+
+button {
+  margin: 5px;
+  padding: 3px;
+  font-size: 15px;
+  cursor: pointer;
+}
+
+input{
+  margin: 5px;
+  padding: 3px;
+  font-size: 15px;
+}
 </style>
